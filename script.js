df9e420cc6e065b93c14f8e8b084c31edf676291
phwang93 <98869145+phwang93@users.noreply.github.com>
2022-04-07 18:30:37 -0700

    Finalizing the characters generator

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ const numericCharacters = [ '1', '2','3','4','5','6','7','8','9','0'];
 
 const lowerCaseCharacters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t', 'u','v','w','x','y','z'];
 
+const upperCaseCharacters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+
 let generateBtne1 = document.querySelector("#generate");
 
 
@@ -22,9 +24,37 @@ function writePassword() {
   };
 
   passwordText.value = password;
-
 }
 
+function generatePassword() {
+  let selectedOptions = getUserSelection ()
+
+  if (selectedOptions === undefined)  {
+    return
+  }
+}
+
+if (lowercase) {
+  possibleCharacters = possibleCharacters.concat(lowerCaseCharacters)
+}
+
+if (uppercase) {
+  possibleCharacters = possibleCharacters.concat(upperCaseCharacters)
+}
+
+if (specialChars) { 
+  possibleCharacters = possibleCharacters.concat(specialCaseCharacters)
+}
+
+if (numbers) { 
+  possibleCharacters = possibleCharacters.concat(numericCharacters) 
+}
+
+
+
+let { length, lowercase, uppercase, specialCharacters, numbers }  = selectedOptions;
+let possibleCharacters = [];
+let generatePassword = "";
 
 
 // Add event listener to generate button
