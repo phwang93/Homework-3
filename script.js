c4fe4ec352b98a88a7ef02274dccd428a23ee3f8
phwang93 <98869145+phwang93@users.noreply.github.com>
2022-04-09 09:49:07 -0700

    added more special characters

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Assignment Code
-const specialCharacters = ['!', '#', '@', '$', '%', '^', '&', '*', '~'];
+const specialCharacters = ['!', '#', '@', '$', '%', '^', '&', '*', '~', '[',']','?','+','-',':',';','>','<'];
 
 const numericCharacters = [ '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
 
